1383f0b6d22f8fd1d614c5592b217c9f36c76f24
oluwatimilehindebola <turnertimmmy904@gmail.com>
2025-04-13 12:32:07 +0100

    Preparing to install forge-devops

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
-
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity   ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+// import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
